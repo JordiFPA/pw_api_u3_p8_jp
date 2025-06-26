139ead7e064f4c61c8542bd02560c76d487c0a89
Jordi Pila <jordifpa7@gmail.com>
2025-06-26 08:37:47 -0500

    Modelo de Madurez de Richarson Nivel 0 y 1 Profesor

--- src/main/java/uce/edu/web/api/repository/IProfesorRepoImpl.java
+++ src/main/java/uce/edu/web/api/repository/IProfesorRepoImpl.java
@@ -1,10 +1,13 @@
 package uce.edu.web.api.repository;
 
+import java.util.List;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import uce.edu.web.api.repository.modelo.Profesor;
+import jakarta.persistence.TypedQuery;
 
 @Transactional
 @ApplicationScoped
@@ -18,6 +21,13 @@ public class IProfesorRepoImpl implements IProfesorRepo {
     public Profesor seleccionarPorId(Integer id) {
 
         return this.entityManager.find(Profesor.class, id);
+    }
+
+    @Override
+    public List<Profesor> SeleccionarTodos() {
+        TypedQuery<Profesor> myQuery = this.entityManager.createQuery("SELECT p FROM Profesor p", Profesor.class);
+        return myQuery.getResultList();
+
     }
 
 }
--- src/main/java/uce/edu/web/api/service/IProfesorServiceImpl.java
+++ src/main/java/uce/edu/web/api/service/IProfesorServiceImpl.java
@@ -1,5 +1,7 @@
 package uce.edu.web.api.service;
 
+import java.util.List;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import uce.edu.web.api.repository.IProfesorRepo;
@@ -14,6 +16,11 @@ public class IProfesorServiceImpl implements IProfesorService {
     @Override
     public Profesor buscarPorId(Integer id) {
         return this.profesorRepo.seleccionarPorId(id);
+    }
+
+    @Override
+    public List<Profesor> buscarTodos() {
+      return this.profesorRepo.SeleccionarTodos();
     }
 
 }
